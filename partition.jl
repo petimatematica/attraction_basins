1d2e67d63874f25bfd25016fcf24fc1470c7fd8d
wmd23 <135710449+wmd23@users.noreply.github.com>
2025-08-21 20:54:04 -0300

    Update partition.jl
    function to test the compatibility of the strategies

--- partition.jl
+++ partition.jl
@@ -10,4 +10,21 @@ function partition_rectangle(interval_x, interval_y, n_x, n_y)
     coordinates = [x_points[i]+y_points[j]*im for i in 1:n_x for j in 1:n_y]
 
     return coordinates
+
+end
+
+function test(n_x,n_y)
+    lin = 0
+    col = 0
+    points = []
+    for i in 1:n_x*n_y
+        if i % n_x == 1
+            col = 1
+            lin += 1
+        else
+            col += 1
+        end
+        push!(points, [lin, col])
+    end
+    return points
 end
