5ff1546d01110a8f6dc19d755ac81eef343ac43d
wmd23 <135710449+wmd23@users.noreply.github.com>
2025-08-26 18:31:08 -0300

    Update partition.jl
    rotation issue fixed

--- Novo/partition.jl
+++ Novo/partition.jl
@@ -7,24 +7,8 @@ function partition_rectangle(interval_x, interval_y, n_x, n_y)
     y_points = range(interval_y[1], interval_y[2], length=n_y)
     
     # Gerar o vetor dos chutes
-    coordinates = [x_points[i]+y_points[j]*im for i in 1:n_x for j in 1:n_y]
+    coordinates = [x_points[i]+y_points[j]*im for j in n_y:-1:1 for i in 1:n_x]
 
     return coordinates
 
-end
-
-function test(n_x,n_y)
-    lin = 0
-    col = 0
-    points = []
-    for i in 1:n_x*n_y
-        if i % n_x == 1
-            col = 1
-            lin += 1
-        else
-            col += 1
-        end
-        push!(points, [lin, col])
-    end
-    return points
 end
