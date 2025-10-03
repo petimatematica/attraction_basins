412d3625e8728d0167c3ff2473fd7edffe585ab2
wmd23 <135710449+wmd23@users.noreply.github.com>
2025-10-03 00:39:32 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,8 +1,8 @@
 include("imagegenerate.jl") # incluindo arquivos necessários
 
 # Região retangular
-interval_x = (-6.0,4.0)
-interval_y = (-5.0,3.0)
+interval_x = [-6.0,4.0]
+interval_y = [-5.0,3.0]
 
 # Número de subdivisões
 n_x = 1000; n_y = 1000 
@@ -12,5 +12,5 @@ f=
 df=
 Z=
 
-imagem, iter = image_generator_off(f,df,interval_x, interval_y, n_x, n_y,Z) # invocando a função que gera as imagens
+imagem, iter = image_generator_off(f,df,interval_x,interval_y,n_x,n_y,Z) # invocando a função que gera as imagens
 save("TesteI.png", colorview(RGB, imagem)) # salva uma imagem no formato png com o nome dado na String
