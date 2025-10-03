19afb52646fe02446460278805c0e53cc6cdcfaf
wmd23 <135710449+wmd23@users.noreply.github.com>
2025-10-03 14:25:13 -0300

    Update imagegenerateSusantoKarjanto.jl

--- imagegenerateSusantoKarjanto.jl
+++ imagegenerateSusantoKarjanto.jl
@@ -1,14 +1,26 @@
 using Images # salvar imagens e acesso ao sistema de cores RGB
+using ColorSchemes # Criação de gradientes e acesso a esquema de cores predefinidas
 include("newton.jl") # incluindo o método de Newton 
 
-# f - função holomorfa que desejamos encontrar seus zeros
-# df - derivada complexa de f
-# interval_x, interval_y - intervalos onde seu produto cartesiano gera um região retangular que contém todos os zeros
-# n_x - número de células homogêneas da partição de interval_x
-# n_y - número de células homogêneas da partição de interval_x
+# cria uma imagem das bacias de atração geradas pelo Método de Newton baseando-se no número de iteradas utilizando a metodologia de 
+#  H. Susanto, N. Karjanto, Newton’s method basin of attraction revisited, Appl. Math. Comput. 215 (2009) 1084–1090.
+#----------------------------------------------------------------------------------------------
+#= 
+Variáveis obrigatória:
+- f (Function) - função complexa holomorfa que desejamos mapear as bacias de atração
+- df (Function) - derivada de f
+- interval_x, interval_y (Vector{Float64}) - intervalos onde seu produto cartesiano gera uma região retangular que contém todos os zeros
+- n_x (Int64) - número de células homogêneas da partição de interval_x
+- n_y (Int64) - número de células homogêneas da partição de interval_y
+
+Variáveis opcionais:
+- epsilon (Float64) - tolerância prescrita ao Método de Newton (padrão 10^{-12})
+- iter (Int64) - número máximo de iteradas (padrão 40 iteradas)
+=#
+#----------------------------------------------------------------------------------------------
+
 function image_generator_iter(f,df,interval_x, interval_y, n_x, n_y; epsilon=1.e-12, iter=40)
     imagem = Matrix{RGB{Float64}}(undef,n_y,n_x) # cria uma imagem de resolução n_y por n_x  
-    Iter = Matrix{Int64}(undef,n_y,n_x) # cria um vetor com n_y por n_x entradas que armazenarão o número de iteradas
     for i in 1:n_y
         for j in 1:n_x
             imagem[i,j]=RGB(1.0,1.0,1.0)
@@ -29,21 +41,18 @@ function image_generator_iter(f,df,interval_x, interval_y, n_x, n_y; epsilon=1.e
 		    s,iters = newton_method(z0, f, df;tol=epsilon,max_iter=iter)
 			
 			if ~isnan(s)
-            	imagem[n_y-lin+1,col] = colors[iters/iter]
-            	Iter[n_y-lin+1] = iters
+            	imagem[n_y-lin+1,col] = colors[iters/iter]  # pinta o pixel na cor correspondente
         	else
             	if iters == -1
                 	imagem[n_y-lin+1,col] = RGB(0.0,0.0,0.0)
-                	Iter[n_y-lin+1,col] = iters
            		end
             	if iters == -2
                 	imagem[n_y-lin+1,col] = colors[m]
-                	Iter[n_y-lin+1,col] = iters
             	end
         	end
  	    end	
 	end
 	     
-	return imagem, Iter
-	
+	return imagem
+
 end
